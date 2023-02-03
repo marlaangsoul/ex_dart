4d70caed39972502abfd70e6b85ec001a0699f58
marlaangsoul <101851921+marlaangsoul@users.noreply.github.com>
2023-02-03 23:55:54 +0900

    23/02/03 study update

--- main.dart
+++ main.dart
@@ -1,20 +1,292 @@
 // =============================================
-//3.5 Typedef (void main 바깥에 있음).
+//4.5 Cascade Notation  (void main 바깥에 있음).
+
+class Player {
+  final String name;
+  int xp; // 이렇게 동시에 선언해줄수도 있다.
+  String team;
+  
+  // api 에서 받아온 데이터를 Player class로 전환해보자.
+  // 많은 플러터 앱에서 사용하는 방법.
+  // 'fromJson' 이라는 named constructor를 만들자. 
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+    : name = playerJson['name'], //playerJson Map 안에 있는 키가 'name'이라는 뜻
+      xp = playerJson['xp'],
+      team = playerJson['team'];
+  // 우리가 위에서 한 것은 단지 Player class의 생성과 초기화 작업 뿐.
+  
+  
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
 
 
 // =============================================
 
 void main() {
   
+  
+  
+//   // =============================================
+// //4.4 Recap  (void main 바깥에 있음).
+
+// class Player {
+//   final String name;
+//   int xp; // 이렇게 동시에 선언해줄수도 있다.
+//   String team;
+  
+//   // api 에서 받아온 데이터를 Player class로 전환해보자.
+//   // 많은 플러터 앱에서 사용하는 방법.
+//   // 'fromJson' 이라는 named constructor를 만들자. 
+
+//   Player.fromJson(Map<String, dynamic> playerJson)
+//     : name = playerJson['name'], //playerJson Map 안에 있는 키가 'name'이라는 뜻
+//       xp = playerJson['xp'],
+//       team = playerJson['team'];
+//   // 우리가 위에서 한 것은 단지 Player class의 생성과 초기화 작업 뿐.
+  
+  
+//   void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+
+
+// // =============================================
+
+  
+//   var apiData = [
+//     {
+//       "name" : "lbh",
+//       "team" : "red",
+//       "xp" : 0,
+//     },
+//      {
+//       "name" : "ksk",
+//       "team" : "red",
+//       "xp" : 0,
+//     },
+//      {
+//       "name" : "boss",
+//       "team" : "red",
+//       "xp" : 0,
+//     },
+//   ];
+  
+//   apiData.forEach((playerJson){
+//     var player = Player.fromJson(playerJson);
+//     player.sayHello();
+//   });
+  
+  
+//   // =============================================
+// //4.3 Named Constructors (void main 바깥에 있음).
+
+// class Player {
+//   final String name;
+//   int xp, age; // 이렇게 동시에 선언해줄수도 있다.
+//   String team;
+
+//   Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//     required this.age,
+//   });
+//   // Named constructor parameter 라고 불리운다.
+//   // 
+//   Player.createBluePlayer({
+//     required String name,
+//     required int age,
+//   }) : // 콜론을 통해 Player 클래스(위의 변수들)을 초기화 싵키는 것이다.
+//   this.age = age,     // age는 어규먼트로 받은 age 값.
+//   this.name = name,   // name도 어규먼트로 받은 age 값.
+//   this.team = 'blue', // team은 개발자가 원하는 기본값
+//   this.xp = 0;        // xp도 개발자가 원하는 기본값
+//   // 이런 식으로 named constructor를 만들수 있다.
+//   // 즉, 이건 Player를 초기화 하는 method이다.
+//   // 하지만 보다시피 다른 이름을 가지고 있고, 필요에 따라서
+//   // 다른 인자를 받게 할수도 있다.    
+//   Player.createRedPlayer({
+//     required String name,
+//     required int age,
+//   }) : // 콜론을 통해 Player 클래스(위의 변수들)을 초기화 싵키는 것이다.
+//   this.age = age,     // age는 어규먼트로 받은 age 값.
+//   this.name = name,   // name도 어규먼트로 받은 age 값.
+//   this.team = 'red', // team은 개발자가 원하는 기본값
+//   this.xp = 0; 
+  
+  
+  
+//   void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+
+
+// // =============================================
+  
+//   // 만약 각각 xp를 0로 초기화 시킨 red 팀과 blue 팀을
+//   // 만들고 싶다면 어떻게 해야할까?
+//   // 내가 사용자가 name 과 age 만 보내도록 한다면??
+//   var bluePlayer = Player.createBluePlayer(
+//     name : "lbh",
+//     age : 12,
+//   );
+//   var redPlayer = Player.createRedPlayer(
+//     name : "ksk",
+//     age : 12,
+//   );
+  
+  // =============================================
+//4.2 Named Constructor Parameters (void main 바깥에 있음).
+// Player가 team도 가지고 있다고 가정해보면,
+// 어규먼트의 순서를 맞춰줘야 하는 이 전 강의의 방식은
+// 금방 어려워질것이다.
+// class Player {
+//   final String name;
+//   int xp;
+//   String team;
+//   int age;
+
+//   Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//     required this.age,
+//   });
+
+//   void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+
+// =============================================
+  
+  
+//   var player = Player(
+//     name : "lbh",
+//     xp : 1500,
+//     team : 'red',
+//     age : 12,
+//   );
+//   player.sayHello();
+//   var player2 = Player(
+//     name : "ksk",
+//     xp : 2500,
+//     team : 'blue',
+//     age : 12,
+//   );
+//   player2.sayHello();
+//   // 너무 많은 positional argument가 서로 이웃해 있으면
+//   // 혼란스러워 진다.
+//   // 그래서 플러터는 전적으로 위와 같이 키와 벨류 값을 구별할 수
+//   // 있게 코딩하는 방식이 많다.
+
+  // =============================================
+//4.1 Constructores (void main 바깥에 있음).
+// 형태 1.
+// class Player{
+//   late final String name;
+//   late int xp;
+//   //constructor method(생성자 함수)
+//   //constructor method의 이름은 클래스의 이름과 같아야 한다.
+//   Player(String name, int xp){
+//     this.name = name;
+//     this.xp = xp;
+//   }
+//   void sayHello(){
+//     print("Hi my name is $name");
+//   }
+// }
+
+// 형태 2.
+// constructor 코드를 작성하는 더 나은 방법은,
+// 일단 late를 지우는 것이다.
+// class Player2{
+//   final String name;
+//   int xp;
+//   //constructor method(생성자 함수)
+//   //constructor method의 이름은 클래스의 이름과 같아야 한다.
+
+//   Player2(this.name, this.xp);
+//   //Player1 의 생성자 메서드 코드보다 훨씬 간결.
+//   //위에서 String, int를 선언했으니
+//   //별도의 변수 선언 없이
+//   //각각의 어규먼트를 그대로 받는다.
+//   // 이때는 위치가 중요하다.
+//
+//   void sayHello(){
+//     print("Hi my name is $name");
+//   }
+// }
+
+// =============================================
+
+//   var player = Player("lbh", 1500);
+//   player.sayHello();
+//   var player2 = Player("ksk", 2500);
+//   player2.sayHello();
+
+//   var player3 = Player2("lbh", 1500);
+//   player3.sayHello();
+//   //두번째 방식에서는 어규먼트 순서가 중요하다.
+//   var player4 = Player2("ksk", 2500);
+//   player4.sayHello();
+
+  //4.0 Your First Dart Class (void main 바깥에 있음).
+// Dart에서는 클래스가 가장 중요.
+
+// class Player{
+//   // 클래스를 생성할때는 타입을 꼭 명시해주어야한다.
+//   String name = 'lbh';
+//   int xp = 1500;
+//   // xp를 바꾸지 못하게 하고 싶으면,
+//   // final int xp = 1500; 으로 하면 된다.
+//   void sayHello(){
+//     print("Hi my name is $name");
+//     // 일반적으로 $name에 this가 붙어서
+//     // ${this.name}으로 사용 되지만, Dart에서는 사용 되지
+//     // 않는게 권고 되고 있다.
+//   }
+// }
+// =============================================
+//   var player = Player('lbh', 1500);
+//   여기서 lbh와 1500을 argument(아규먼트)라고 한다.
+//   // 이렇게 호출시 Player 인스턴스가 하나 생성된다.
+//   // var playetr = new Player(); 로 써도 되지만
+//   // 굳이 그럴 필요는 없다.
+//   player.name; //.(점)을 통해 name이나xp에 접근 할수 있다.
+//   print(player.name);
+//   player.name = 'lalalala';
+//   print(player.name);
+//   player.sayHello();
+
+  // =============================================
+//3.5 Typedef (void main 바깥에 있음).
+//typedef는 자료형에 alias를 붙일 수 있게 해준다.
+//alias는 별칭 as 같은거.
+// typedef ListOfInts = List<int>;
+
+// ListOfInts reverseListOfnumbers(ListOfInts list){
+//   var reversed = list.reversed;
+//   return reversed.toList();
+// }
+// =============================================
+
+//   print(reverseListOfnumbers([1,2,3]));
+
   // =============================================
 //3.4 QQ Operator(void main 바깥에 있음).
 // ??
 // 소문자를 대문자로 바꿔주는 함수
 // name 값이 null 일때 방법 1.
-// String capitalizeName(String? name) => 
+// String capitalizeName(String? name) =>
 //   name != null ? name.toUpperCase() : 'ANOW';
 // name 값이 null이 아닐때 방법 2.
-// String capitalizeName2(String? name) => 
+// String capitalizeName2(String? name) =>
 //   name?.toUpperCase() ?? 'ANOW';
 // 설명
 // left ?? right
@@ -23,13 +295,11 @@ void main() {
 
 // =============================================
 
-  
 //   capitalizeName('lbh');
 //   capitalizeName(null); // 이걸 사용할때 String에 ? 처리
 //   capitalizeName2('lbh');
 //   capitalizeName2(null);
 
-  
 // ?=
 //   String? name;
 //   name ??= 'lbh';
