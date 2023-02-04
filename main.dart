2fae1507da14f6c708b0ae6abad7c2878fe835b1
marlaangsoul <101851921+marlaangsoul@users.noreply.github.com>
2023-02-04 20:04:13 +0900

    23/02/04 study update

--- main.dart
+++ main.dart
@@ -1,34 +1,290 @@
 // =============================================
-//4.5 Cascade Notation  (void main 바깥에 있음).
-
-class Player {
-  final String name;
-  int xp; // 이렇게 동시에 선언해줄수도 있다.
-  String team;
-  
-  // api 에서 받아온 데이터를 Player class로 전환해보자.
-  // 많은 플러터 앱에서 사용하는 방법.
-  // 'fromJson' 이라는 named constructor를 만들자. 
-
-  Player.fromJson(Map<String, dynamic> playerJson)
-    : name = playerJson['name'], //playerJson Map 안에 있는 키가 'name'이라는 뜻
-      xp = playerJson['xp'],
-      team = playerJson['team'];
-  // 우리가 위에서 한 것은 단지 Player class의 생성과 초기화 작업 뿐.
-  
-  
-  void sayHello() {
-    print("Hi my name is $name");
-  }
-}
-
+//5.0 Conclusions
+// 필수라고 생각하는 Dart의 기본기.
+// Dart는 객체 지향 프로그래밍이기 떄문에 클래스를 중점으로 배웠고
+// 플러터 역시 객체지향 프로그래밍 프레임워크이다.
+// 
 
 // =============================================
 
 void main() {
   
+//   // =============================================
+// //4.9 Mixins  (void main 바깥에 있음).
+// // Mixin 플러터에서 자주 사용 되는 개념
+// // 생성자가 없는 클래스
+// // 전 강의에서 Human은 생성자가 있었다.
+// // 그래서 Player를 만들때는 super를 써야했다.
+// // 클래스에 프로퍼티를 추가하거나 생성할때 사용.
+
+// class Human {
+//   final String name;
+//   Human({required this.name}); // 생성자 함수
+//   void sayHello() {
+//     print("$name car is rolls_royce Spectre");
+//   }
+// }
+
+// class Strong {
+//   final double strenghtLevel = 1500.99;
+//   //Strong은 클래스고 어떤 생성자도 가지고 있지 않다.
+// }
+
+// class QuickRunner {
+//   void runQuick() {
+//     print("ruuuuun!");
+//   }
+// }
+
+// class Tall{
+//   final double height = 1.99;
+// }
+
+// enum Team { red, blue }
+
+// // with로 Quick와 Strong 클래스에 있는 프로퍼티와 메소드를
+// // Player에 담아 줄것이다.
+// // 요약 : 다른 클래스의 프로퍼티와 메소드를 그냥 긁어 오는 것.
+// // 이떄 상속 받을 필요가 없어서 쉽게 사용.
+// // 앞으로 플러터나 플로터 플로그인에 사용할때 자주 사용.
+
+
+// //Mixin의 핵심은 여러 클래스에 재사용이 가능하다는 점.
+// class Player with Strong, QuickRunner, Tall {
+//   final Team team;
+
+//   Player({
+//     required this.team,
+//     required String name,
+//   }) : super(name: name);
+
+//   @override
+//   void sayHello() {
+//     super.sayHello();
+//     print('and my car is ${team}');
+//   }
+// }
+
+// // 재사용이 가능하다.
+// class Horse with Strong, QuickRunner {}
+
+// class Kid with QuickRunner {}
+
+// // extend를 하게 되면 확장(상속)한 그 클래스는 부모 클래스가 되고
+// // 자식 클래스는 부모 클래스를 super를 통해 접근할수 있고
+// // 그 순간 부모 클래스의 인스턴스가 된다.
+// // Mixin은 다르다. with를 쓰게 되면
+// // 역할은 단순히 Mixin 내부의 프로퍼티와 메소드를 가져오는 것뿐.
+// // 부모 클래스가 되거나 하지 않는다. 그저 뺏어서 사용 하는 것 뿐.
+// // Mixin의 조건은 생성자가 없는 클래스여야 한다는 것. 
+
+// // =============================================
+  
+//   var player = Player(
+//   team: Team.red,
+//   );
+  
+//   player.runQuick();
   
   
+//   // =============================================
+// //4.8 Inheritance (void main 바깥에 있음).
+// // 상속. 플러터에서는 가끔씩만 사용 되지만
+// // 꼭 알아야 하는 중요한 개념.
+// class Human{
+//   final String name;
+//   Human({required this.name});// 생성자 함수
+//   void sayHello(){
+//     print("$name car is rolls_royce Spectre");
+//   }
+// }
+
+// enum Team{red, blue}
+
+// class Player extends Human{
+//   // Human 가 가지고 있는 모든 것을 가지게 한다.
+//   final Team team;
+
+//     // 생성자 함수에도 위에 Human클래스의 메소드를 가져온다.
+//     // namde argument 를 사용하여
+//     // Player의 생성자 함수를 만들자.
+//   Player({
+//     required this.team,
+//     //Human클래스에서 String타입인 name 이라는 필수요소를 받겠다.
+//     required String name,
+//   }) : super(name: name); //super 클래스는 확장한 부모 클래스를 의미하는 것.
+//   // name을 super 생성자에 전달해주면 됨.
+//   // super라는 키워드를 통해서(확장을 한) 부모 클래스와 상호작용
+//   // 할수 있게 도와줌
+//   // super에 name을 전달해주면 이 클래스를 전달한 name과 함께 호출됨
+
+//   @override
+//   // Human에서 온 sayHello를 우리가 직접 만든
+//   // 메소드로 override(대체) 하는 것
+//   void sayHello(){
+//     super.sayHello();
+//     print('and my car is ${team}');
+//   }
+//   // 우린 지금 Human에 있는 걸들을 가져와서 사용하고 있음.
+//   // super의 힘. 상속(확장)한 부모 클래스의 프로퍼티에 접근하거나
+//   // 메소드를 호출할수 있게 해줌.
+// }
+//   // 여기서 가장 중요한 것은 확장한 부모 클래스가 생성자를 포함하고 있는데
+//   // 그 클래스를 다른 어떤 곳에서 사용하려면 필요한 값을 전달해야하고
+//   // 그 부모 클래스의 생성자를 호출해줘야한다.
+//   // 자바 C언어, 자바 스크립트등 이러한 형태로 상속을 한다.
+
+// // =============================================
+
+//   var player = Player(
+//     team: Team.red,
+//     name:'lbh',
+//   );
+//   player.sayHello();
+
+//   // =============================================
+// //4.7 Abstract Classes (void main 바깥에 있음).
+// // 추상화 클래스는 다른 클래스 들이 직접 구현해야 하는
+// // 메소드들을 모아 놓은 일종의 청사진이라고 볼수 있다.
+// // 다른 클래스들이 어떤 청사진을 가지고 있어야하는지
+// // 정의해주는 역할
+// abstract class Human {
+//   void walk();
+// }
+// // 추상화 클래스에 좋은 점.
+// // 수많은 청사진에 메소드의 이름과 반환타입만 정해서
+// // 정의 할수 있다.
+// // 플러터에서 자주 사용하지는 않지만 유용하다.
+// // ex) walk(가로 안에) 파라미터를 설정할수 있고,
+// // 다른 클래스에서 extedns(상속, 확장) 할수 있다.
+
+// enum Team {red, blue}
+// enum XPLevel {beginner, medium, master}
+
+// class Player extends Human {
+//   String name;
+//   XPLevel xp;
+
+//   Team team;
+//    Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//   });
+
+//   void walk{
+//     print('My car is rolls_royce Spectre');
+//   }
+
+//     void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+
+// class Coach extends Human{
+//   void walk(){
+//     print('My wife car is rolls_royce Spectre');
+//   }
+// }
+// // 추상화 클래스를 Extends 받았을 때는 특정 메소드
+// // 구현을 강제한다. 그래서 Player와 Coach 클래스가
+// // walk 메소드를 가지고 있다고 확신할수 있다.
+// // 하지만 Coach클래스에서 walk 메소드를 보면 알수 있듯이,
+// // 다른 방식으로 구현이 가능하다.
+
+// // =============================================
+
+//   // =============================================
+// //4.6 Enums (void main 바깥에 있음).
+// // 이눔은 플러터에서 많이 사용하고,
+// // 우리가 바보같은 실수를 하지 않게 도와준다.
+// enum Team {red, blue}
+// enum XPLevel {beginner, medium, master}
+// // 텍스트 형태로 쓰는 것이 아니다.
+// // 문자열이든 변수명이든 사오간 없다.
+// class Player {
+//   String name;
+//   XPLevel xp;
+//   //String team;
+//   //을 아래와 같이 바꾼다.
+//   Team team;
+//    Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//   });
+//     void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+// // =============================================
+
+//     var lbh = Player(name: 'lbh', xp: XPLevel.medium, team: Team.red);
+//   lbh.name = 'las';
+//   lbh.xp = XPLevel.medium;
+//   lbh.team = Team.red;
+
+//   var lbh = Player(name: 'lbh', xp: XPLevel.medium, team: Team.red)
+//   ..name = 'las'
+//   ..xp = XPLevel.medium
+//   ..team = Team.red;
+
+//   var lbh = Player(name: 'lbh', xp: XPLevel.medium, team: Team.blue);
+//   var potato = lbh
+//   ..name = 'las'
+//   ..xp = XPLevel.medium
+//   ..team = Team.blue
+//   ..sayHello();
+
+  // =============================================
+// //4.5 Cascade Notation  (void main 바깥에 있음).
+
+// class Player {
+//   String name;
+//   int xp;
+//   String team;
+
+//   // api 에서 받아온 데이터를 Player class로 전환해보자.
+//   // 많은 플러터 앱에서 사용하는 방법.
+//   // 'fromJson' 이라는 named constructor를 만들자.
+
+//    Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//   });
+
+//   void sayHello() {
+//     print("Hi my name is $name");
+//   }
+// }
+
+// // =============================================
+
+//   var lbh = Player(name: 'lbh', xp: 1200, team: 'red');
+//   lbh.name = 'las';
+//   lbh.xp = 1200000;
+//   lbh.team = 'red';
+
+//   // 위와 같이 lbh를 여러번 쓰는 반복적인 행위를 할 필요없이
+//   // Cascade operator 를 쓰면 간단하다.
+
+//   //아래 예시.
+//   var lbh = Player(name: 'lbh', xp: 1200, team: 'red')
+//   ..name = 'las'
+//   ..xp = 1200000
+//   ..team = 'red';  // 앞에 .. 중 앞에 . 이 위의 var 뒤에 쓴 lbh를 뜻한다.
+
+//   //위와 아래는 같은 역할이고, 같은 로직이다.
+//   // 플로터에서 자주 사용 되는 것이다.
+
+//   var lbh = Player(name: 'lbh', xp: 1200, team: 'red');
+//   var potato = lbh
+//   ..name = 'las'
+//   ..xp = 1200000
+//   ..team = 'red'
+//   ..sayHello(); //이것도 가능
+
 //   // =============================================
 // //4.4 Recap  (void main 바깥에 있음).
 
@@ -36,27 +292,24 @@ void main() {
 //   final String name;
 //   int xp; // 이렇게 동시에 선언해줄수도 있다.
 //   String team;
-  
+
 //   // api 에서 받아온 데이터를 Player class로 전환해보자.
 //   // 많은 플러터 앱에서 사용하는 방법.
-//   // 'fromJson' 이라는 named constructor를 만들자. 
+//   // 'fromJson' 이라는 named constructor를 만들자.
 
 //   Player.fromJson(Map<String, dynamic> playerJson)
 //     : name = playerJson['name'], //playerJson Map 안에 있는 키가 'name'이라는 뜻
 //       xp = playerJson['xp'],
 //       team = playerJson['team'];
 //   // 우리가 위에서 한 것은 단지 Player class의 생성과 초기화 작업 뿐.
-  
-  
+
 //   void sayHello() {
 //     print("Hi my name is $name");
 //   }
 // }
 
-
 // // =============================================
 
-  
 //   var apiData = [
 //     {
 //       "name" : "lbh",
@@ -74,13 +327,12 @@ void main() {
 //       "xp" : 0,
 //     },
 //   ];
-  
+
 //   apiData.forEach((playerJson){
 //     var player = Player.fromJson(playerJson);
 //     player.sayHello();
 //   });
-  
-  
+
 //   // =============================================
 // //4.3 Named Constructors (void main 바깥에 있음).
 
@@ -96,7 +348,7 @@ void main() {
 //     required this.age,
 //   });
 //   // Named constructor parameter 라고 불리운다.
-//   // 
+//   //
 //   Player.createBluePlayer({
 //     required String name,
 //     required int age,
@@ -108,7 +360,7 @@ void main() {
 //   // 이런 식으로 named constructor를 만들수 있다.
 //   // 즉, 이건 Player를 초기화 하는 method이다.
 //   // 하지만 보다시피 다른 이름을 가지고 있고, 필요에 따라서
-//   // 다른 인자를 받게 할수도 있다.    
+//   // 다른 인자를 받게 할수도 있다.
 //   Player.createRedPlayer({
 //     required String name,
 //     required int age,
@@ -116,18 +368,15 @@ void main() {
 //   this.age = age,     // age는 어규먼트로 받은 age 값.
 //   this.name = name,   // name도 어규먼트로 받은 age 값.
 //   this.team = 'red', // team은 개발자가 원하는 기본값
-//   this.xp = 0; 
-  
-  
-  
+//   this.xp = 0;
+
 //   void sayHello() {
 //     print("Hi my name is $name");
 //   }
 // }
 
-
 // // =============================================
-  
+
 //   // 만약 각각 xp를 0로 초기화 시킨 red 팀과 blue 팀을
 //   // 만들고 싶다면 어떻게 해야할까?
 //   // 내가 사용자가 name 과 age 만 보내도록 한다면??
@@ -139,7 +388,7 @@ void main() {
 //     name : "ksk",
 //     age : 12,
 //   );
-  
+
   // =============================================
 //4.2 Named Constructor Parameters (void main 바깥에 있음).
 // Player가 team도 가지고 있다고 가정해보면,
@@ -164,8 +413,7 @@ void main() {
 // }
 
 // =============================================
-  
-  
+
 //   var player = Player(
 //     name : "lbh",
 //     xp : 1500,
